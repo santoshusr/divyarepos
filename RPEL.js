c325d71a62c19ab3ed32c66e3fee7fc644546ae5
santoshusr <s.k.santosh@hotmail.com>
2017-02-04 14:57:40 +0530

    First commit

---
+++ RPEL.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+var x=0, y;
+x=x+1;
+y=99
+
+do {x++;
+console.log(x+y);
+
+}while (x < 9);
+
---
+++ main.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+var http = require("http");
+http.createServer(function(req,rpo)
+{
+	rpo.writeHead(200,{'Content-Type': 'text/plain'});
+	rpo.end("http server created");
+}).listen(8081);
+
+/* Blocking code 
+var fs= require("fs");
+var data =fs.readFileSync('input.txt');
+console.log(data.toString());
+console.log("program ended"); */
+
+
+/* Unblocking code 
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+}); */
+
+
+
+console.log("Program Ended");
+
+console.log('Server running at http://127.0.0.1:8081/');
